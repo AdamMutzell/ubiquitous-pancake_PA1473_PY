cb7dc51966613d32849792bd4cd4c702740a41db
Oliver <Powerking2000@gmail.com>
2022-04-20 21:45:21 +0200

    Get_File()

--- project/Colour_Calibrator.py
+++ project/Colour_Calibrator.py
@@ -4,7 +4,6 @@ from numpy import save
 from pybricks.tools import wait
 from pybricks.ev3devices import ColorSensor
 
-saved_colours =  open("savedColours.txt", "w")
 def Calibrate_Colours(colours,sensor):
     """
     Enter a calibraton mode where the user must press a button on the brick and set the colour value for each individual colour reading.
@@ -12,19 +11,24 @@ def Calibrate_Colours(colours,sensor):
     Returns a dictionary of colours and saves to txt file
     """
     calibrated_colours = colours.copy()
+    saved_colours =  open("savedColours.txt", "w")
 
     for label,colour in colours.items():
-        print("set colour for: "+label)
-        running = True
-        while running:
-            if brick.buttons.pressed(): #if any button is pressed
-                running = False
-        selected_colour = colour#sensor.color()
-        #^Uncomment for testing^
-        calibrated_colours[i] = selected_colour
+        #print("set colour for: "+label)
+        #running = True
+        #while running:
+        #    if brick.buttons.pressed(): #if any button is pressed
+        #        running = False
+        #        brick.speaker.beep()
+        selected_colour = colour#sensor.hsv()
+        #^Uncomment for testing robot^
+        calibrated_colours[label] = selected_colour
         saved_colours.write(str(selected_colour)+"\n")
     saved_colours.close()
     print(calibrated_colours)
     return calibrated_colours
-
-    
+def Get_File():
+    saved_colours = open("savedColours.txt", "r")
+    colours = saved_colours.read().split("\n")
+    print(colours)
+    saved_colours.close()
