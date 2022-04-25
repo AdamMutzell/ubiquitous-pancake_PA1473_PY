4e7c1323f7f2eebefd1e4648cd76689fc57d9928
AdamMutzell <admt21@student.bth.se>
2022-04-25 13:23:11 +0200

    fixing function names

--- project/adam.py
+++ project/adam.py
@@ -1,5 +1,4 @@
 import sys
-from tkinter import Button
 from turtle import color, left, right
 import __init__
 import Colour_Manager
@@ -7,18 +6,13 @@ import Colour_Manager
 import math,time
 
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, TouchSensor, ColorSensor, UltrasonicSensor, Button
+from pybricks.ev3devices import Motor, TouchSensor, ColorSensor, UltrasonicSensor
 from pybricks.parameters import Port, Color, Direction
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait
 from pybricks.media.ev3dev import SoundFile
 
-if EV3Brick.Button().left:
-    #drive towards red warehouse
-    print("Driving towards Red Warehouse")
-elif EV3Brick.Button().right:
-    #drive towards blue warehouse
-    print("Driving towards Blue Warehouse")
+btn = EV3Brick.Button()
 
 
 #START
