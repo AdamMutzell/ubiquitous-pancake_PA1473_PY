9e78326757361d7b758e230ec50ac52377ba8436
Uplink036 <69390820+Uplink036@users.noreply.github.com>
2022-04-27 13:45:54 +0200

    Bug hunting

--- project/Colour_Manager.py
+++ project/Colour_Manager.py
@@ -1,36 +1,36 @@
-from sqlite3 import Row
 from pybricks.hubs import EV3Brick as brick
 from pybricks.parameters import Button
-from numpy import save
 from pybricks.tools import wait
 from pybricks.ev3devices import ColorSensor
-from pybricks.parameters import Color 
+from pybricks.parameters import Color
 
-import ast
+#import ast
 
-def Calibrate_Colours(colours,sensor):
+
+def Calibrate_Colours(colours, sensor):
     """
     Enter a calibraton mode where the user must press a button on the brick and set the colour value for each individual colour reading.
 
     Returns a dictionary of colours and saves to txt file
     """
     calibrated_colours = colours.copy()
-    saved_colours =  open("savedColours.txt", "w")
+    saved_colours = open("savedColours.txt", "w")
 
-    for label,colour in colours.items():
+    for label, colour in colours.items():
         #print("set colour for: "+label)
         #running = True
-        #while running:
+        # while running:
         #    if brick.buttons.pressed(): #if any button is pressed
         #        running = False
         #        brick.speaker.beep()
-        selected_colour = colour#sensor.hsv()
-        #^Uncomment for testing robot^
+        selected_colour = colour  # sensor.hsv()
+        # ^Uncomment for testing robot^
         saved_colours.write(label+":"+str(colour)+"\n")
     saved_colours.close()
     calibrated_colours
     return calibrated_colours
-    
+
+
 def Get_File():
     colours = {}
     saved_colours = open("savedColours.txt", "r")
@@ -42,18 +42,20 @@ def Get_File():
     print(colours)
     saved_colours.close()
 
+
 def get_colour(light_sensor):
     """
     Returns the colour of the ground the robot is looking at
     """
-    return light_sensor.color()
+    return light_sensor.color.hsv()
 
-def get_area(colours,light_sensor):
+
+def get_area(colours, light_sensor):
     threshold = 15
     current_colour = get_colour(light_sensor)
     for zone in colours:
         if current_colour == colours[zone]:
-            #change: check if get_colour is within threshold of colours[zone], read colour will never be the same as saved colour 
+            # change: check if get_colour is within threshold of colours[zone], read colour will never be the same as saved colour
             current_zone = zone
-                # area has been switched
+            # area has been switched
     return current_zone
--- project/test/test_main.py
+++ project/test/test_main.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env pybricks-micropython
+from main import drive, crane_movement, crane_pickup
+
+
+def main():  # Main Class
+    # Testing the crane
+    # drive()
+    pickupstatus = True
+    max_angle = crane_movement(Crane_motor, 1, 50)
+    min_angle = crane_movement(Crane_motor, -1, 50)
+
+    crane_pickup(Crane_motor, TRUCK, Front_button, -1000, max_angle, min_angle)
+
+    detect_item_fail(pickupstatus, Front_button)
+
+
+if __name__ == '__main__':  # Keep this!
+    sys.exit(main())
